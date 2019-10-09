3c0b8c1e05b1828f610369f0397468142bf0009f
Rene Angeles <reneang17@gmail.com>
2019-10-08 21:03:33 -0500

    Added batcher, deleted unnecesary files.

---
+++ network/batcher.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def batcher(X, y=None, batch_size=64):
+    """ batch generator """
+    n_samples = X.shape[1]
+    for i in np.arange(0, n_samples, batch_size):
+        start, end = i, min(i+batch_size, n_samples)
+        if y is not None:
+            yield X[:, start:end], y[start:end]
+        else:
+            yield X[:, start:end]
