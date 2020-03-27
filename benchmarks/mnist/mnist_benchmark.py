Fixed bug, normalization on Activation_SoftMax

benchmarks/mnist/mnist_benchmark.py
@@ -53,18 +53,17 @@ from main_class import *
 from layers import *
 from loss_functions import *
 
+print('MultiClassCrossEntropy')
 md=NeuralNetwork(MultiClassCrossEntropy)
 np.random.seed(1)
 
 n_x = 784    # num_px * num_px * 3
-
-md.add(Dense(100, input_shape=(n_x,), initializer = 'normal'))
+lr = 0.05    # num_px * num_px * 3
+md.add(Dense(100, input_shape=(n_x,), initializer = 'normal', lr = lr))
 md.add(Activation('relu'))
-
-md.add(Dense(200, initializer = 'normal'))
+md.add(Dense(200, initializer = 'normal', lr = lr))
 md.add(Activation('relu'))
-
-md.add(Dense(10, initializer = 'normal'))
+md.add(Dense(10, initializer = 'normal', lr = lr))
 md.add(Activation_SoftMax())
 
 #train
@@ -83,3 +82,34 @@ def accuracy(test_x, test_y):
 ## Evalaution
 print('Training accuarecy: {}'.format(accuracy(train_x , train_y)))
 print('Test accuarecy: {}'.format(accuracy(test_x , test_y)))
+print('Training loss: {}'.format(hist[0][-1]))
+
+
+print('Using')
+md=NeuralNetwork(SoftmaxCrossEntropy)
+np.random.seed(1)
+lr = 0.05
+n_x = 784    # num_px * num_px * 3
+md.add(Dense(100, input_shape=(n_x,), initializer = 'normal', lr = lr))
+md.add(Activation('relu'))
+md.add(Dense(200, initializer = 'normal', lr = lr))
+md.add(Activation('relu'))
+md.add(Dense(10, initializer = 'normal', lr = lr))
+
+#train
+hist = md.fit(train_x, train_y, n_epochs=25, batch_size=32)
+
+def softmax(x):
+        e_x = np.exp(x )
+        return e_x / np.sum(e_x, axis=0, keepdims=True)
+
+def accuracy(test_x, test_y):
+    preds = softmax(md.predict(test_x))
+    preds = np.array([y for y in np.argmax(preds, axis=0)]).squeeze()
+    test_y_ = np.array([y for y in np.argmax(test_y, axis=0)]).squeeze()
+    return np.mean(preds == test_y_)
+
+## Evalaution
+print('Training accuarecy: {}'.format(accuracy(train_x , train_y)))
+print('Test accuarecy: {}'.format(accuracy(test_x , test_y)))
+print('Training loss: {}'.format(hist[0][-1]))

network/loss_functions.py
@@ -38,7 +38,7 @@ class CrossEntropy(Loss):
     def loss(self, y, AL):
         # Avoid division by zero
         AL = np.clip(AL, 1e-15, 1 - 1e-15)
-        return (- y * np.log(AL) - (1 - y) * np.log(1 - AL)) /(y.shape[-1])
+        return (- y * np.log(AL) - (1 - y) * np.log(1 - AL)) #/(y.shape[-1])#(1)normalize in main
 
     def acc(self, y, AL):
         return accuracy_score(y[0], AL[0]>=0.5)
@@ -47,7 +47,7 @@ class CrossEntropy(Loss):
         # Avoid division by zero
         AL = np.clip(AL, 1e-15, 1 - 1e-15)
         #print  ((- (y / AL) + (1 - y) / (1 - AL) ).shape,'cross-function output dA')
-        return (- (y / AL) + (1 - y) / (1 - AL)) /(y.shape[-1])
+        return (- (y / AL) + (1 - y) / (1 - AL)) #/(y.shape[-1])#(2)normalize when updating grad
 
 
 
@@ -55,18 +55,15 @@ class MultiClassCrossEntropy(Loss):
     def __init__(self): pass
 
     def loss(self, y, AL):
-        # Avoid division by zero
-        #AL = np.clip(AL, 1e-15, 1 - 1e-15)
-        return -np.sum(y * np.log(AL) ,axis= 0, keepdims = True) /(y.shape[-1])
-    #def acc(self, y, AL):
-    #    return accuracy_score(y[0], AL[0]>=0.5)
+        return -np.sum(y * np.log(AL) ,axis= 0, keepdims = True) #/(y.shape[-1])#(1)normalize in main
+
     def gradient(self, y, AL):
         # Avoid division by zero
         AL = np.clip(AL, 1e-15, 1 - 1e-15)
 
         assert(AL.shape == (-(y / AL)).shape)
-        #print  ((- (y / AL) + (1 - y) / (1 - AL) ).shape,'cross-function output dA')
-        return - (y / AL)  /(y.shape[-1])
+
+        return - (y / AL) # /(y.shape[-1])#(2)normalize when updating grad
 
 class SoftmaxCrossEntropy(Loss):
     def __init__(self): pass
@@ -78,12 +75,12 @@ class SoftmaxCrossEntropy(Loss):
         assert Z_aux ==Z.shape
 
         log_e_Z = Z- np.log(np.sum( np.exp(Z), axis=0, keepdims=True))
-        return (-np.sum( y * log_e_Z ,axis= 0)) /(Z.shape[-1])
-    #def acc(self, y, AL):
-    #return accuracy_score(y[0], AL[0]>=0.5)
+        return (-np.sum( y * log_e_Z ,axis= 0))  # (1)normalize in main
+
+
     def gradient(self, y, Z):
         Z -= np.max(Z, axis = 0, keepdims=True)
         p = np.exp(Z)/ np.sum( np.exp(Z), axis=0, keepdims=True)
 
         # Avoid division by zero
-        return  (-y + p) /(Z.shape[-1])
+        return  (-y + p)  #(2)Normalize when updating grad

network/main_class.py
@@ -38,7 +38,7 @@ class NeuralNetwork():
         # If not first layer added then set the input shape
         # to the output shape of the last added layer
         if self.layers:
-            layer.set_input_shape(shape=self.layers[-1].output_shape())
+            layer.set_input_shape(shape=self.layers[-1].get_output_shape())
 
         # If the layer has weights that needs to be initialized
         if hasattr(layer, 'initialize'):
@@ -52,7 +52,7 @@ class NeuralNetwork():
     def train_on_batch(self, X, y):
         """ Forward/backward on batch """
         y_pred = self._forward(X)
-        loss = np.sum(self.loss_function.loss(y, y_pred))
+        loss = np.mean(self.loss_function.loss(y, y_pred))#(1)mean normalizes
         acc = self.loss_function.acc(y, y_pred)
         # Calculate the gradient of the loss function wrt y_pred
         loss_grad = self.loss_function.gradient(y, y_pred)